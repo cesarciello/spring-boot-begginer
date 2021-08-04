0e2b3d82843df8652c694aa60e3db6dbd686b7d3
Cesar Ciello <cesarciello15@gmail.com>
2021-08-03 22:17:02 -0300

    Service layer, component registration

--- src/main/java/ciello/arsenal/course/resources/UserResource.java
+++ src/main/java/ciello/arsenal/course/resources/UserResource.java
@@ -1,20 +1,32 @@
 package ciello.arsenal.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ciello.arsenal.course.entities.User;
+import ciello.arsenal.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Cesar Ciello", "cesar@mail.com", "1111111", "123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = userService.findAll();
+		return ResponseEntity.ok().body(users);
 	}
 
+	@GetMapping(value = "{id}")
+	public ResponseEntity<User> finById(@PathVariable Long id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
 }
